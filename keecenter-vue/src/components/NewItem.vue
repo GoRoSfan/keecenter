a89756869b467d7646099d1ef29f511e263463c4
GoRoSfan <olsngrs@gmail.com>
2020-02-09 19:21:47 +0200

    add new component

---
+++ keecenter-vue/src/components/NewItem.vue
@@ -0,0 +1,108 @@
+<template>
+    <article>
+        <header class="news-header">
+            <cite>{{NewTitle}}</cite>
+            <time>{{NewPostDate}}</time>
+        </header>
+        <div class="news-main">
+            <img :src="host + NewImage" alt="Фотографія до новини" class="news-image">
+            <pre class="news-description">{{NewDescription}}</pre>
+        </div>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: "NewItem",
+        props: ['NewTitle', 'NewPostDate', 'NewImage', 'NewDescription', 'IsDateAvailable'],
+        data() {
+            return {
+                host: process.env.VUE_APP_API_URL
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    article {
+        display: flex;
+
+        flex-direction: column;
+
+        margin: 2vh 0;
+    }
+
+    header {
+        display: flex;
+
+        flex-flow: row wrap;
+        justify-content: space-between;
+
+        padding: 0.8vh 2vw;
+
+        color: #3914AF;
+
+        border-radius: 2vw 2vw 0 0;
+        border-bottom: #FFD300 solid 1px;
+
+        box-shadow: inset 0 -1px #646464;
+
+        background-color: #FFD300;
+    }
+
+    header cite {
+        align-self: center;
+
+        font-style: normal;
+        font-size: 1.5rem;
+    }
+
+    aside header cite {
+        font-size: 1.2rem;
+    }
+
+    header time {
+        align-self: start;
+
+        font-size: 1rem;
+    }
+
+    aside time {
+        display: none;
+    }
+
+    .news-main {
+        padding: 1rem 1.5vw;
+
+        color: #FFE773;
+
+        background-color: #311491;
+
+        box-shadow: 2px 3px 1.5rem #ad8946;
+    }
+
+    .news-main img {
+        width: 30%;
+        float: right;
+        padding: 0 0 2rem 1rem;
+    }
+
+    aside .news-main img {
+        width: 90%;
+        float: none;
+        padding-left: 0;
+    }
+
+    .news-main pre {
+        font-family: inherit;
+        font-size: 1.2rem;
+        text-align: left;
+
+        white-space: pre-wrap;
+        word-break: normal;
+    }
+
+    aside .news-main pre {
+        font-size: 0.85rem;
+    }
+</style>
--- keecenter-vue/src/views/AllNews.vue
+++ keecenter-vue/src/views/AllNews.vue
@@ -2,21 +2,30 @@
     <HomeSlot #main>
         <div class="context-header"><h2>Новини центру</h2></div>
         <div class="all-news-container">
-            <article
-                    v-for="one_new in news_list"
-                    :key="one_new"
-                    class="news-container"
-            >
-                <header class="news-header">
-                    <cite>{{one_new.title}}</cite>
-                    <time>{{one_new.post_date}}</time>
-                </header>
-                <div class="news-main">
-                    <img :src="host + one_new.image" alt="Фотографія до новини" class="news-image">
-                    <pre class="news-description">{{one_new.description}}</pre>
-                </div>
-                <footer class="news-footer" @click="go_to_detail(host + one_new.detail)">Детальніше</footer>
-            </article>
+            <NewItem
+                    v-for="(one_new, index) in news_list"
+                    :key="index"
+                    :new-title="one_new.title"
+                    :new-post-date="one_new.post_date"
+                    :new-image="one_new.image"
+                    :new-description="one_new.description"
+                    :is-date-available="true"
+            />
+<!--            <article-->
+<!--                    v-for="(one_new, index) in news_list"-->
+<!--                    :key="index"-->
+<!--                    class="news-container"-->
+<!--            >-->
+<!--                <header class="news-header">-->
+<!--                    <cite>{{one_new.title}}</cite>-->
+<!--                    <time>{{one_new.post_date}}</time>-->
+<!--                </header>-->
+<!--                <div class="news-main">-->
+<!--                    <img :src="host + one_new.image" alt="Фотографія до новини" class="news-image">-->
+<!--                    <pre class="news-description">{{one_new.description}}</pre>-->
+<!--                </div>-->
+<!--                <footer class="news-footer" @click="go_to_detail(host + one_new.detail)">Детальніше</footer>-->
+<!--            </article>-->
         </div>
         <mu-flex justify-content="center" style="margin: 2vh 0;">
             <mu-pagination
@@ -24,13 +33,14 @@
                     :current.sync="current_page"
                     :page-size="page_size"
                     @change="page_change"
-            ></mu-pagination>
+            />
         </mu-flex>
     </HomeSlot>
 </template>
 
 <script>
     import HomeSlot from './Home';
+    import NewItem from "../components/NewItem";
     import $ from 'jquery'
 
     export default {
@@ -38,6 +48,7 @@
 
         components: {
             HomeSlot,
+            NewItem
         },
 
         data() {
@@ -82,9 +93,9 @@
                 });
             },
 
-            go_to_detail: function (link) {
-                location.href = link;
-            },
+            // go_to_detail: function (link) {
+            //     location.href = link;
+            // },
         },
     };
 </script>
@@ -102,88 +113,21 @@
         -webkit-flex-flow: column nowrap;
     }
 
-    .all-news-container article {
-        display: -webkit-box;
-        display: -moz-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
+    /*.all-news-container footer {*/
+    /*    text-align: center;*/
 
-        flex-direction: column;
-        flex-wrap: nowrap;
-        -webkit-flex-flow: column nowrap;
+    /*    padding: 1.5vh 2vw;*/
+    /*    border-radius: 0 0 2vw 2vw;*/
 
-        margin: 2vh 0;
-    }
+    /*    color: #3914AF;*/
+    /*    font-size: 1.4rem;*/
 
-    .all-news-container header {
-        display: -webkit-box;
-        display: -moz-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
+    /*    background-color: #FFD300;*/
 
-        -webkit-flex-flow: row wrap;
-        flex-flow: row wrap;
-        justify-content: space-between;
+    /*    cursor: pointer;*/
+    /*}*/
 
-        padding: 0.8vh 2vw;
-
-        color: #3914AF;
-
-        border-radius: 2vw 2vw 0 0;
-        border-bottom: #FFD300 solid 1px;
-
-        -webkit-box-shadow: inset 0 -1px #646464;
-        -moz-box-shadow: inset 0 -1px #646464;
-        box-shadow: inset 0 -1px #646464;
-
-        background-color: #FFD300;
-    }
-
-    .all-news-container .news-header cite {
-        align-self: center;
-
-        font-style: normal;
-        font-size: 1.5rem;
-    }
-
-    .all-news-container .news-header time {
-        align-self: start;
-
-        font-size: 1rem;
-    }
-
-    .all-news-container .news-main {
-        padding: 1.5vh 1.5vw;
-
-        color: #FFE773;
-        text-align: left;
-        font-size: 1.2rem;
-
-        background-color: #311491;
-    }
-
-    .all-news-container .news-main img {
-        width: 30%;
-        float: right;
-    }
-
-    .all-news-container footer {
-        text-align: center;
-
-        padding: 1.5vh 2vw;
-        border-radius: 0 0 2vw 2vw;
-
-        color: #3914AF;
-        font-size: 1.4rem;
-
-        background-color: #FFD300;
-
-        cursor: pointer;
-    }
-
-    .all-news-container footer:hover {
-        background-color: #FFAA00;
-    }
+    /*.all-news-container footer:hover {*/
+    /*    background-color: #FFAA00;*/
+    /*}*/
 </style>

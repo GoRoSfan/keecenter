f7bac2955ef2afc86703477fe8560aa4f4c95f03
GoRoSfan <olsngrs@gmail.com>
2020-03-09 09:28:37 +0200

    add hash / save_model function

--- public/admin.py
+++ public/admin.py
@@ -11,8 +11,13 @@ admin.site.register(Partners)
 
 class NewsAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'description', 'visitors_type')
-    fields = ('title', 'description', ('visitors_type', 'content_types'))
+    list_display = ('title', 'description', 'visitors_type', 'image_hash')
+    fields = ('title', 'description', ('visitors_type', 'content_types'), 'image')
+
+    def save_model(self, request, obj: News, form, change):
+        super().save_model(request, obj, form, change)
+        obj.image_hash = request.META['HTTP_ORIGIN'] + obj.image.url
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(News, NewsAdmin)

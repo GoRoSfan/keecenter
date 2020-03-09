04bf7ab804a312727d434732a878c6672ed2a778
GoRoSfan <olsngrs@gmail.com>
2020-03-09 09:32:05 +0200

    take ALLOWED_HOSTS & BOT_TOKEN

--- keecenter/prod_settings.py
+++ keecenter/prod_settings.py
@@ -2,6 +2,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ALLOWED_HOSTS = ['keecenter.herokuapp.com']
+
 DEBUG = False
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -11,3 +13,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+BOT_TOKEN = os.environ.get('TG_BOT')

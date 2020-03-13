024a1f2da9573cb6f121795df7e2201be822da50
GoRoSfan <olsngrs@gmail.com>
2020-03-13 19:33:54 +0200

    make dev version of storing image

--- public/admin.py
+++ public/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import News, ContentTypesNews, ActivityTypesClubs, Events, Clubs, Contacts,\
     Legals, Employees, Partners
 
-from .bot import send_photo
+from .bot import send_image
+from keecenter.settings import ALLOWED_HOSTS
 
 
 admin.site.register(ContentTypesNews)
@@ -14,13 +15,16 @@ admin.site.register(Partners)
 class NewsAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'description', 'visitors_type', 'image_hash')
-    fields = ('title', 'description', ('visitors_type', 'content_types'), 'image')
+    fields = ('title', 'description', ('visitors_type', 'content_types'), 'image', 'image_hash')
 
     def save_model(self, request, obj: News, form, change):
-        super().save_model(request, obj, form, change)
-        image_link = request.META['HTTP_ORIGIN'] + obj.image.url
-        obj.image_hash = send_photo(image_link, obj._meta.verbose_name_plural)
-        super().save_model(request, obj, form, change)
+        if '127.0.0.1' in ALLOWED_HOSTS:
+            super().save_model(request, obj, form, change)
+        else:
+            super().save_model(request, obj, form, change)
+            image_link = request.META['HTTP_ORIGIN'] + obj.image.url
+            obj.image_hash = send_image(image_link, obj._meta.verbose_name_plural)
+            super().save_model(request, obj, form, change)
 
 
 admin.site.register(News, NewsAdmin)
